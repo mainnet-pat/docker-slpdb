5fdf683838ed759956870520356e710130cac5e5
mainnet-pat <mainnet_pat@protonmail.com>
2020-12-29 22:05:22 +0300

    Regtest adaptations for mainnet.cash

--- run-script.sh
+++ run-script.sh
@@ -7,17 +7,17 @@
 # Set the MongoDB IP address and port. By default uses the MongoDB Docker
 # container bundled in the docker-compose.yml file.
 #export db_url=mongodb://localhost:27017
-export db_url=mongodb://172.17.0.1:12301
+export db_url=mongodb://mongodb:27017
 
 
 # Set the full node IP address and port
-export rpc_host=172.17.0.1
-export rpc_port=8332
-export rpc_user=bitcoin
+export rpc_host=bitcoind
+export rpc_port=18443
+export rpc_user=alice
 export rpc_pass=password
 
-export zmq_incoming_host=172.17.0.1
-export zmq_incoming_port=28332
+export zmq_incoming_host=bitcoind
+export zmq_incoming_port=8332
 
 # Turn off graph search
 export enable_graph_search=0
@@ -26,7 +26,7 @@ export enable_graph_search=0
 export zmq_outgoing_enable=0
 
 # Set the telemtry name for this node
-export telemetry_advertised_host=trout-docker-unedited
+export telemetry_advertised_host=mainnet-cash-regtest
 
 npm start
 #tsc && node --max_old_space_size=8192 index run --startHeight 604260
